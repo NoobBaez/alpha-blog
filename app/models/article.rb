569333da60ff0ab07f2ca816ca97419fb307c0e9
Haniel <haniel_0101@hotmail.com>
2017-10-24 05:39:27 +0000

    Add controller and views for create and show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-     #validates :title, presence: true, :length { minimum: 3, maximum: 50 }
-     #validates :description, presence: true, :length { minimum: 10, maximum: 300 }
+     validates :title, presence: true, length: { minimum: 7, maximum: 15 }
+     validates :description, presence: true, length: { minimum: 1, maximum: 100 }
 end
